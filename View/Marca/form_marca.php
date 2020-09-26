215587e5f65aa1ecf24d75ba0a8c83e35ecea073
Maria Regina <172979@upf.br>
2020-09-25 21:06:59 -0300

    Interface

--- View/Marca/form_marca.php
+++ View/Marca/form_marca.php
@@ -5,38 +5,154 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <title>Manutenção de Marca</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>CRUD Marca</title>
+  <!-- MDB icon -->
+  <link rel="icon" href="../img/eletronica_raio.png" type="image/x-icon">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+  <!-- Google Fonts Roboto -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="../css/bootstrap.min.css">
+  <!-- Material Design Bootstrap -->
+  <link rel="stylesheet" href="../css/mdb.min.css">
+  <!-- Your custom styles (optional) -->
+  <link rel="stylesheet" href="../css/style.css">
   </head>
   <body>
-    <div class="container">
-      <h1>Manutenção de Marca</h1>
 
-      <form class="form" action="<?php echo $acao; ?>" method="post">
-        <div class="form-group">
-          <label for="">Marca</label>
-          <input class="form-control" type="text" name="nomeMarca"
-              value="<?php if(isset($registro)) echo $registro['nomeMarca']; ?>" required>
+  <!-- Barra de Navegação -->
+  <header>
+      <!--Navbar-->
+      <nav class="navbar navbar-expand-lg navbar-dark peach-gradient fixed-top">
+        <div class="container">
+          <!-- Navbar brand -->
+          <a class="navbar-brand" href="#">
+            <img src="../img/eletronica_NomeSemBorda.png" height="30" alt="mdb logo">
+          </a>
+
+          <!-- Collapse button -->
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
+            aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <!-- Collapsible content -->
+          <div class="collapse navbar-collapse" id="basicExampleNav">
+
+            <!-- Links -->
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="../home.php">
+                  <i class="fas fa-home"></i>Home
+                  <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              
+              <!-- Dropdown -->
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                  aria-haspopup="true" aria-expanded="false">
+                  <i class="fas fa-desktop"></i> Ordem de Serviço</a>
+                <div class="dropdown-menu dropdown-primary orange accent-2" aria-labelledby="navbarDropdownMenuLink">
+                  <a class="dropdown-item" href="#">Cadastrar</a>
+                  <a class="dropdown-item" href="#">Consultar</a>
+                </div>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                  aria-haspopup="true" aria-expanded="false">
+                  <i class="far fa-file-alt"></i>Cadastros Básicos</a>
+                <div class="dropdown-menu dropdown-primary orange accent-2" aria-labelledby="navbarDropdownMenuLink">
+                  <a class="dropdown-item" href="home.php?controller=marca">Marca</a>
+                  <a class="dropdown-item" href="home.php?controller=servico">Serviço</a>
+                  <a class="dropdown-item" href="home.php?controller=peca">Peça</a>
+                  <a class="dropdown-item" href="home.php?controller=statusServico">Status Serviço</a>
+                  <a class="dropdown-item" href="home.php?controller=tipoPessoa">Tipo Pessoa</a>
+                  <!-- <a class="dropdown-item" href="#">Equipamento</a>
+                  <a class="dropdown-item" href="#">Pessoa</a> -->
+                </div>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                  aria-haspopup="true" aria-expanded="false">
+                  <i class="far fa-file-alt"></i> Relatórios</a>
+                <div class="dropdown-menu dropdown-primary orange accent-2" aria-labelledby="navbarDropdownMenuLink">
+                  <a class="dropdown-item" href="#">Gerar Relatórios</a>
+                </div>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="index.html">
+                  <i class="fas fa-sign-out-alt"></i>Sair
+                </a>
+              </li>
+
+            </ul>
+            <!-- Links -->
+          </div>
+          <!-- Collapsible content -->
+
         </div>
-        <div class="form-group">
-          <label for="">Observação</label>
-          <input class="form-control" type="text" name="observacaoMarca"
-              value="<?php if(isset($registro)) echo $registro['observacaoMarca']; ?>">
+      </nav>
+      <!--/.Navbar-->
+
+      <!-- Mask -->
+        <div id="intro" class="view">
+          <div class="container-fluid full-bg-img mask rgba-black-strong d-flex align-items-center
+            justify-content-center">
+
+            <div class="row d-flex justify-content-center">
+              <div class="col-md-10 text-center"> 
+                <div class="container">
+                  <h1>Manutenção de Marca</h1>
+
+                  <form class="form" action="<?php echo $acao; ?>" method="post">
+                    <div class="form-group">
+                      <label for="">Marca</label>
+                      <input class="form-control" type="text" name="nomeMarca"
+                       value="<?php if(isset($registro)) echo $registro['nomeMarca']; ?>" required>
+                    </div>
+                    <div class="form-group">
+                      <label for="">Observação</label>
+                      <input class="form-control" type="text" name="observacaoMarca"
+                       value="<?php if(isset($registro)) echo $registro['observacaoMarca']; ?>">
+                    </div>
+                    <button class="btn btn-info" type="submit">Enviar</button>
+                  </form>
+                </div>               
+              </div>
+            </div>
+          </div>
         </div>
-        <button class="btn btn-info" type="submit">Enviar</button>
-      </form>
+      <!-- Mask -->
+    </header>
+  <!-- Barra de Navegação -->
+    
+  <!-- Footer -->
+  <footer class="page-footer font-small peach-gradient">
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2020 Copyright:
+      <a href="#"> Maria Regina Cerbaro</a>
     </div>
+    <!-- Copyright -->
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </footer>
+  <!-- Footer -->
+
+  <!-- jQuery -->
+  <script type="text/javascript" src="../js/jquery.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="../js/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="../js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="../js/mdb.min.js"></script>
+  <!-- Your custom scripts (optional) -->
+  <script type="text/javascript"></script>
   </body>
 </html>
--- View/home.php
+++ View/home.php
@@ -1,29 +1,138 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Administrador</title>
+  <!-- MDB icon -->
+  <link rel="icon" href="../img/eletronica_raio.png" type="image/x-icon">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+  <!-- Google Fonts Roboto -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="../css/bootstrap.min.css">
+  <!-- Material Design Bootstrap -->
+  <link rel="stylesheet" href="../css/mdb.min.css">
+  <!-- Your custom styles (optional) -->
+  <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <!-- Barra de Navegação -->
+    <header>
+      <!--Navbar-->
+      <nav class="navbar navbar-expand-lg navbar-dark peach-gradient fixed-top">
+        <div class="container">
+          <!-- Navbar brand -->
+          <a class="navbar-brand" href="#">
+            <img src="../img/eletronica_NomeSemBorda.png" height="30" alt="mdb logo">
+          </a>
 
-    <title>Teste TCC!</title>
-  </head>
-  <body>
-    <h1>Teste TCC!</h1>
-    <nav>
-      <a href="?controller=marca">Marca</a> | 
-      <a href="?controller=peca">Peça</a> | 
-      <a href="?controller=tipoPessoa">Tipo Pessoa</a> |
-      <a href="?controller=servico">Serviço</a> |
-      <a href="?controller=statusServico">Status Serviço</a>
-    </nav>
+          <!-- Collapse button -->
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
+            aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-  </body>
+          <!-- Collapsible content -->
+          <div class="collapse navbar-collapse" id="basicExampleNav">
+
+            <!-- Links -->
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">
+                  <i class="fas fa-home"></i>Home
+                  <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              
+              <!-- Dropdown -->
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                  aria-haspopup="true" aria-expanded="false">
+                  <i class="fas fa-desktop"></i> Ordem de Serviço</a>
+                <div class="dropdown-menu dropdown-primary orange accent-2" aria-labelledby="navbarDropdownMenuLink">
+                  <a class="dropdown-item" href="#">Cadastrar</a>
+                  <a class="dropdown-item" href="#">Consultar</a>
+                </div>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                  aria-haspopup="true" aria-expanded="false">
+                  <i class="far fa-file-alt"></i>Cadastros Básicos</a>
+                <div class="dropdown-menu dropdown-primary orange accent-2" aria-labelledby="navbarDropdownMenuLink">
+                  <a class="dropdown-item" href="?controller=marca">Marca</a>
+                  <a class="dropdown-item" href="?controller=servico">Serviço</a>
+                  <a class="dropdown-item" href="?controller=peca">Peça</a>
+                  <a class="dropdown-item" href="?controller=statusServico">Status Serviço</a>
+                  <a class="dropdown-item" href="?controller=tipoPessoa">Tipo Pessoa</a>
+                  <!-- <a class="dropdown-item" href="#">Equipamento</a>
+                  <a class="dropdown-item" href="#">Pessoa</a> -->
+                </div>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                  aria-haspopup="true" aria-expanded="false">
+                  <i class="far fa-file-alt"></i> Relatórios</a>
+                <div class="dropdown-menu dropdown-primary orange accent-2" aria-labelledby="navbarDropdownMenuLink">
+                  <a class="dropdown-item" href="#">Gerar Relatórios</a>
+                </div>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="index.html">
+                  <i class="fas fa-sign-out-alt"></i>Sair
+                </a>
+              </li>
+
+            </ul>
+            <!-- Links -->
+          </div>
+          <!-- Collapsible content -->
+
+        </div>
+      </nav>
+      <!--/.Navbar-->
+
+      <!-- Mask -->
+        <div id="intro" class="view">
+          <div class="container-fluid full-bg-img mask rgba-black-strong d-flex align-items-center
+            justify-content-center">
+
+            <div class="row d-flex justify-content-center">
+              <div class="col-md-10 text-center">                
+              </div>
+            </div>
+          </div>
+        </div>
+      <!-- Mask -->
+    </header>
+  <!-- Barra de Navegação -->
+
+  <!-- Footer -->
+  <footer class="page-footer font-small peach-gradient">
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2020 Copyright:
+      <a href="#"> Maria Regina Cerbaro</a>
+    </div>
+    <!-- Copyright -->
+
+  </footer>
+  <!-- Footer -->
+
+  <!-- jQuery -->
+  <script type="text/javascript" src="../js/jquery.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="../js/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="../js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="../js/mdb.min.js"></script>
+  <!-- Your custom scripts (optional) -->
+  <script type="text/javascript"></script>
+
+</body>
 </html>
